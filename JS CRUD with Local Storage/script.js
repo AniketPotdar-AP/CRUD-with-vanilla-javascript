c057add96de19b5a3eb31ad5e5a5fbb4a6f3fae2
Aniket Potdar <aniketpanditpotdar@gmail.com>
2023-09-24 22:31:54 +0530

    Upload Files

---
+++ JS CRUD with Local Storage/script.js
@@ -0,0 +1,150 @@
+// *****************************************
+//          Get All Contact List
+// *****************************************
+
+document.onload = showData();
+
+function showData() {
+  var contactList;
+
+  if (localStorage.getItem("contactList") == null) {
+    contactList = [];
+  } else {
+    contactList = JSON.parse(localStorage.getItem("contactList"));
+  }
+
+  var html = "";
+
+  contactList.forEach(function (element, index) {
+    html += "<tr>";
+    html += "<td>" + element.firstName + "</td>";
+    html += "<td>" + element.lastName + "</td>";
+    html += "<td>" + element.contactNum + "</td>";
+    html +=
+      '<td><button class="btn btn-warning" onClick="updateData(' +
+      index +
+      ')">Edit</button> <button class="btn btn-danger mx-2" onClick="deleteData(' +
+      index +
+      ')">Delete</button></td>';
+    html += "</tr>";
+  });
+
+  document.querySelector("#contactList tbody").innerHTML = html;
+}
+
+// *****************************************
+//         Add Data in Contact List
+// *****************************************
+
+function addData() {
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+  var contactNum = document.getElementById("contactNum").value;
+
+  if (validate()) {
+    var contactList;
+
+    if (localStorage.getItem("contactList") == null) {
+      contactList = [];
+    } else {
+      contactList = JSON.parse(localStorage.getItem("contactList"));
+    }
+  }
+
+  contactList.push({
+    firstName: firstName,
+    lastName: lastName,
+    contactNum: contactNum,
+  });
+
+  localStorage.setItem("contactList", JSON.stringify(contactList));
+
+  showData();
+  resetForm();
+}
+
+// *****************************************
+//         Update Data in Contact List
+// *****************************************
+
+function updateData(index) {
+  document.getElementById("Add").style.display = "none";
+  document.getElementById("Update").style.display = "inline";
+
+  var contactList;
+
+  if (localStorage.getItem("contactList") == null) {
+    contactList = [];
+  } else {
+    contactList = JSON.parse(localStorage.getItem("contactList"));
+  }
+
+  document.getElementById("firstName").value = contactList[index].firstName;
+  document.getElementById("lastName").value = contactList[index].lastName;
+  document.getElementById("contactNum").value = contactList[index].contactNum;
+
+  document.querySelector("#Update").onclick = function () {
+    if (validate()) {
+      contactList[index].firstName = document.getElementById("firstName").value;
+      contactList[index].lastName = document.getElementById("lastName").value;
+      contactList[index].contactNum =
+        document.getElementById("contactNum").value;
+
+      localStorage.setItem("contactList", JSON.stringify(contactList));
+      showData();
+      resetForm();
+
+      document.getElementById("Add").style.display = "inline";
+      document.getElementById("Update").style.display = "none";
+    }
+  };
+}
+
+// *****************************************
+//         Delete Data in Contact List
+// *****************************************
+
+function deleteData(index) {
+  var contactList;
+
+  if (localStorage.getItem("contactList") == null) {
+    contactList = [];
+  } else {
+    contactList = JSON.parse(localStorage.getItem("contactList"));
+  }
+
+  contactList.splice(index, 1);
+  localStorage.setItem("contactList", JSON.stringify(contactList));
+
+  showData();
+}
+
+// **********************************************
+//        Check if Form is Valid or Not
+// **********************************************
+
+function validate() {
+  isValid = true;
+  if (
+    document.getElementById("firstName").value == "" ||
+    document.getElementById("lastName").value == "" ||
+    document.getElementById("contactNum").value == ""
+  ) {
+    isValid = false;
+    alert("Please Fill all required Fields");
+  } else {
+    isValid = true;
+  }
+  return isValid;
+}
+
+// **********************************************
+//        Reset All Form Values to Null
+// **********************************************
+
+function resetForm() {
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("contactNum").value = "";
+  selectedRow = null;
+}
---
+++ JS CRUD with Rest Api/script.js
@@ -0,0 +1,139 @@
+// *****************************************
+//          Get All Contact List
+// *****************************************
+
+document.onload = getData();
+
+function getData() {
+  fetch("https://average-fish-sundress.cyclic.app/allContacts", {
+    method: "GET",
+    headers: { "Content-type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      var html = "";
+      response.forEach((contact) => {
+        var id = contact._id;
+        html += `<tr>`;
+        html += `<td> ${contact.firstName} </td>`;
+        html += `<td> ${contact.lastName} </td>`;
+        html += `<td> ${contact.phoneNumber} </td>`;
+        html += `<td>`;
+        html += `<button class="btn btn-warning" onClick="editData('${id}')">Edit</button>`;
+        html += `<button class="btn btn-danger mx-2" onClick="deleteData('${id}')">Delete</button>`;
+        html += `</td>`;
+        html += `</tr>`;
+      });
+      document.getElementById("tbody").innerHTML = html;
+    });
+}
+
+// *****************************************
+//         Add Data in Contact List
+// *****************************************
+
+function addData() {
+  if (validate()) {
+    fetch("https://average-fish-sundress.cyclic.app/addContact", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        phoneNumber: document.getElementById("phoneNumber").value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        getData();
+        resetForm();
+      });
+  }
+}
+
+// *****************************************
+//         Update Data in Contact List
+// *****************************************
+var dataID;
+
+function editData(id) {
+  fetch(`https://average-fish-sundress.cyclic.app/contact/${id}`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      dataID = id;
+      document.getElementById("firstName").value = data.firstName;
+      document.getElementById("lastName").value = data.lastName;
+      document.getElementById("phoneNumber").value = data.phoneNumber;
+
+      document.getElementById("Add").style.display = "none";
+      document.getElementById("Update").style.display = "inline";
+    });
+}
+
+function updateData() {
+  if (validate()) {
+    fetch("https://average-fish-sundress.cyclic.app/updateContact", {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        _id: dataID,
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        phoneNumber: document.getElementById("phoneNumber").value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        getData();
+        resetForm();
+        document.getElementById("Add").style.display = "inline";
+        document.getElementById("Update").style.display = "none";
+      });
+  }
+}
+
+// *****************************************
+//         Delete Data in Contact List
+// *****************************************
+
+function deleteData(id) {
+  fetch(`https://average-fish-sundress.cyclic.app/removeContact/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      getData();
+    });
+}
+
+// **********************************************
+//        Check if Form is Valid or Not
+// **********************************************
+
+function validate() {
+  isValid = true;
+  if (
+    document.getElementById("firstName").value == "" ||
+    document.getElementById("lastName").value == "" ||
+    document.getElementById("phoneNumber").value == ""
+  ) {
+    isValid = false;
+    alert("Please Fill all required Fields");
+  } else {
+    isValid = true;
+  }
+  return isValid;
+}
+
+// **********************************************
+//        Reset All Form Values to Null
+// **********************************************
+
+function resetForm() {
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("phoneNumber").value = "";
+  selectedRow = null;
+}
